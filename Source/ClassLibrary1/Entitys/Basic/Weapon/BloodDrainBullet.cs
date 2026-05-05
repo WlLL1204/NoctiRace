4a21e59da9a74ccb079b2bee696e4f6f3b281be9
RFujisiro <ryomafujishiro@gmail.com>
2026-05-05 20:58:44 +0900

    ノクティプロジェクト

---
+++ Source/ClassLibrary1/Entitys/Basic/Weapon/BloodDrainBullet.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using UnityEngine;
+
+
+namespace NoctiRace
+{
+    internal class BloodDrainBullet:Bullet
+    {
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            base.Impact(hitThing, blockedByShield);
+            Pawn victim= hitThing as Pawn;
+
+        }
+
+    }
+}
